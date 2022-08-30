042c32a756dbf9cf9dc04c7a008079c6f138022f
Rente6 <84778080+Rente6@users.noreply.github.com>
2022-08-30 09:34:04 -0300

    Add files via upload

---
+++ config/database.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+
+const conexao = async()=>{
+    const atlas = await mongoose.connect('mongodb+srv://user3tri:user3tri@fiaptecnico.b869f.mongodb.net/test')
+}
+
+const modelo = mongoose.Schema({
+    estilo:String,
+    imagem:String,
+    titulo:String,
+    texto:String,
+    publicado:{type:Date, default:Date.now}
+})
---
+++ routes/formulario.js
@@ -0,0 +1,8 @@
+module.exports = (app)=>{
+    app.get('/formulario',(req,res)=>{
+        res.render('formulario.ejs')
+    })
+    app.post('/formulario',(req,res)=>{
+        res.send(req.body)
+    })
+}
